872406386de891dd331befb6a7191b5c3fa942e9
majdishami <majdi01@yahoo.com>
2025-02-20 18:03:13 -0800

    f

---
+++ types/types_db-schema.d.ts
@@ -0,0 +1,10 @@
+declare module '@db/schema' {
+  export const users: any;
+  export const insertUserSchema: any;
+  export const categories: any;
+  export const insertCategorySchema: any;
+  export const transactions: any;
+  export const bills: any;
+  export const insertTransactionSchema: any;
+  export type SelectUser = any;
+}
---
+++ types/types_replit-vite-plugin-runtime-error-modal.d.ts
@@ -0,0 +1,4 @@
+declare module '@replit/vite-plugin-runtime-error-modal' {
+  const runtimeErrorOverlay: any;
+  export = runtimeErrorOverlay;
+}
