59856fb4e47c1bf12a3e25f2dea32623ebdd96b5
majdishami <majdi01@yahoo.com>
2025-02-21 11:38:35 -0800

    f

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 "use strict";
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
+    if (k2 === undefined) k = k;
     var desc = Object.getOwnPropertyDescriptor(m, k);
     if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
       desc = { enumerable: true, get: function() { return m[k]; } };
     }
     Object.defineProperty(o, k2, desc);
 }) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
+    if (k2 === undefined) k = k;
     o[k2] = m[k];
 }));
 var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
@@ -23,19 +23,18 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+    return mod && mod.__esModule ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const vite_1 = require("vite");
 const plugin_react_1 = __importDefault(require("@vitejs/plugin-react"));
-const vite_plugin_shadcn_theme_json_1 = __importDefault(require("@replit/vite-plugin-shadcn-theme-json"));
 const path_1 = __importStar(require("path"));
 const vite_plugin_runtime_error_modal_1 = __importDefault(require("@replit/vite-plugin-runtime-error-modal"));
 const url_1 = require("url");
 const __filename = (0, url_1.fileURLToPath)(import.meta.url);
 const __dirname = (0, path_1.dirname)(__filename);
 exports.default = (0, vite_1.defineConfig)({
-    plugins: [(0, plugin_react_1.default)(), (0, vite_plugin_runtime_error_modal_1.default)(), (0, vite_plugin_shadcn_theme_json_1.default)()],
+    plugins: [(0, plugin_react_1.default)(), (0, vite_plugin_runtime_error_modal_1.default)()],
     resolve: {
         alias: {
             "@db": path_1.default.resolve(__dirname, "db"),
