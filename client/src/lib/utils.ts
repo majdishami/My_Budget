74176c4ebae4c1224d3857137c0afbf1749d5969
majdi01 <39325428-majdi01@users.noreply.replit.com>
2025-02-25 22:19:31 +0000

    User checkpoint: Add Sonner toaster component and utility functions.  Update dependencies and fix TypeScript version.

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -5,27 +5,18 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+export function formatCurrency(amount: number) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  }).format(amount)
 }
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+export function getCurrentDate() {
+  return new Date()
 }
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+export function generateId() {
+  return Math.random().toString(36).substr(2, 9)
 }
