0f0af7b28c448d049e1b9f3f0f6913633668d81c
majdishami <majdi01@yahoo.com>
2025-02-26 13:48:05 -0800

    f

---
+++ sequelize.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false, // Disable logging; default: console.log
+});
+
+module.exports = sequelize;
