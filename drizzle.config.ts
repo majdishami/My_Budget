3bdb4d3c9ac3386fbfdfd246c91886c44d7c5bb2
majdishami <majdi01@yahoo.com>
2025-02-26 13:33:22 -0800

    f

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,8 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   schema: "./src/schema",
   out: "./drizzle",
-  driver: "d1-http",
+  driver: "pg",
   dbCredentials: {
-    accountId: process.env.ACCOUNT_ID,
-    databaseId: process.env.DATABASE_ID,
-    token: process.env.DATABASE_TOKEN,
+    connectionString: process.env.DATABASE_URL ?? (() => { throw new Error("DATABASE_URL is not defined") })(),
   },
 });
