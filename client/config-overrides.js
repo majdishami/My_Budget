d27ce1d96c1a9cb38bafe174fd690a377ecf163f
majdishami <majdi01@yahoo.com>
2025-02-26 17:33:36 -0800

    f

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,29 +1,27 @@
 const webpack = require('webpack');
 
-module.exports = {
-  webpack: function override(config) {
-    config.resolve.fallback = {
-      zlib: require.resolve('browserify-zlib'),
-      querystring: require.resolve('querystring-es3'),
-      path: require.resolve('path-browserify'),
-      crypto: require.resolve('crypto-browserify'),
-      fs: false,
-      stream: require.resolve('stream-browserify'),
-      http: require.resolve('stream-http'),
-      net: false,
-      tls: false,
-      url: require.resolve('url/'),
-      util: require.resolve('util/'),
-      buffer: require.resolve('buffer/')
-    };
+module.exports = function override(config) {
+  config.resolve.fallback = {
+    zlib: require.resolve('browserify-zlib'),
+    querystring: require.resolve('querystring-es3'),
+    path: require.resolve('path-browserify'),
+    crypto: require.resolve('crypto-browserify'),
+    fs: false,
+    stream: require.resolve('stream-browserify'),
+    http: require.resolve('stream-http'),
+    net: false,
+    tls: false,
+    url: require.resolve('url/'),
+    util: require.resolve('util/'),
+    buffer: require.resolve('buffer/')
+  };
 
-    config.plugins = (config.plugins || []).concat([
-      new webpack.ProvidePlugin({
-        process: 'process/browser',
-        Buffer: ['buffer', 'Buffer'],
-      }),
-    ]);
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ]);
 
-    return config;
-  },
+  return config;
 };
