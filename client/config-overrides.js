3a4a16fd4be43bc4c22d1141234b82a473d7b8e6
majdishami <majdi01@yahoo.com>
2025-02-27 11:50:40 -0800

    f

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,31 +1,30 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = function override(config) {
-  config.resolve.fallback = {
-    zlib: require.resolve('browserify-zlib'),
-    querystring: require.resolve('querystring-es3'),
-    path: require.resolve('path-browserify'),
-    crypto: require.resolve('crypto-browserify'),
-    fs: false,
-    stream: require.resolve('stream-browserify'),
-    http: require.resolve('stream-http'),
-    net: false,
-    tls: false,
-    url: require.resolve('url/'),
-    util: require.resolve('util/'),
-    buffer: require.resolve('buffer/'),
-    dns: false,
-    async_hooks: false,
-    pg_native: false,
-  };
-
+module.exports = function override(config, env) {
+  // Add or modify Webpack plugins
   config.plugins = (config.plugins || []).concat([
     new webpack.ProvidePlugin({
       process: 'process/browser',
       Buffer: ['buffer', 'Buffer'],
     }),
   ]);
+
+  // Add or modify Webpack resolve
+  config.resolve = {
+    ...config.resolve,
+    fallback: {
+      zlib: require.resolve('browserify-zlib'),
+      querystring: require.resolve('querystring-es3'),
+      path: require.resolve('path-browserify'),
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      http: require.resolve('stream-http'),
+      url: require.resolve('url/'),
+      util: require.resolve('util/'),
+      buffer: require.resolve('buffer/'),
+    },
+  };
 
   return config;
 };
