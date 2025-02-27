804eca13291d94f413f7c22f82c498d1b3569af8
majdishami <majdi01@yahoo.com>
2025-02-27 11:30:57 -0800

    f

--- client/config-overrides.cjs
+++ client/config-overrides.cjs
@@ -1,6 +1,31 @@
 const path = require('path');
+const webpack = require('webpack');
 
-(async () => {
-  const override = await import(path.resolve(__dirname, 'client', 'config-overrides.js'));
-  module.exports = override.default;
-})();
+module.exports = function override(config) {
+  config.resolve.fallback = {
+    zlib: require.resolve('browserify-zlib'),
+    querystring: require.resolve('querystring-es3'),
+    path: require.resolve('path-browserify'),
+    crypto: require.resolve('crypto-browserify'),
+    fs: false,
+    stream: require.resolve('stream-browserify'),
+    http: require.resolve('stream-http'),
+    net: false,
+    tls: false,
+    url: require.resolve('url/'),
+    util: require.resolve('util/'),
+    buffer: require.resolve('buffer/'),
+    dns: false,
+    async_hooks: false,
+    pg_native: false,
+  };
+
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ]);
+
+  return config;
+};
