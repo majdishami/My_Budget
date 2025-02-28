30d5683c39cb8df1022fe98798beb87b4d4e9f81
majdishami <majdi01@yahoo.com>
2025-02-27 16:48:39 -0800

    f

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
             presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
             plugins: [
               require.resolve('react-refresh/babel'),
-            ].filter(Boolean),
+            ],
           },
         },
       },
